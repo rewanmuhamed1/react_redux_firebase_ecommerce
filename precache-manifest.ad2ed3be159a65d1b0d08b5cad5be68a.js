6b7296785e7ca464a4379ef255bf3a301a3c36df
rewan mohamed <rewan1208@gmail.com>
2021-04-06 13:36:03 +0200

    Updates

---
+++ precache-manifest.ad2ed3be159a65d1b0d08b5cad5be68a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "706c0816d812da90c8fba3ce0a288e6d",
+    "url": "/react_redux_firebase_ecommerce/index.html"
+  },
+  {
+    "revision": "cf2d670c8c5760132258",
+    "url": "/react_redux_firebase_ecommerce/static/css/main.3034a6bb.chunk.css"
+  },
+  {
+    "revision": "9b3e96329e379cae528a",
+    "url": "/react_redux_firebase_ecommerce/static/js/2.d90bab7c.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/react_redux_firebase_ecommerce/static/js/2.d90bab7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2d670c8c5760132258",
+    "url": "/react_redux_firebase_ecommerce/static/js/main.9aeb62ac.chunk.js"
+  },
+  {
+    "revision": "27bd4d03038f9c693243",
+    "url": "/react_redux_firebase_ecommerce/static/js/runtime-main.be20ac8d.js"
+  }
+]);
